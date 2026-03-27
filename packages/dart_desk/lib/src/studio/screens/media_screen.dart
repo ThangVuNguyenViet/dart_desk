b803b5587a74f3426fbec513b99c16e9adf86da6
thangvu <thangvnv0806@gmail.com>
2026-03-28 01:26:19 +0700

    feat(dart_desk): implement MediaScreen

--- packages/dart_desk/lib/src/studio/screens/media_screen.dart
+++ packages/dart_desk/lib/src/studio/screens/media_screen.dart
@@ -1,10 +1,20 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../media/browser/media_browser.dart';
+import '../config/studio_config.dart';
 
 @RoutePage()
 class MediaScreen extends StatelessWidget {
   const MediaScreen({super.key});
 
   @override
-  Widget build(BuildContext context) => const SizedBox.shrink();
+  Widget build(BuildContext context) {
+    final config = GetIt.I<StudioConfig>();
+    return MediaBrowser(
+      dataSource: config.dataSource,
+      mode: MediaBrowserMode.standalone,
+    );
+  }
 }
