66fa56cd0f193bc32d06176a417fda1fc266cd0d
thangvu <thangvnv0806@gmail.com>
2026-03-31 18:55:12 +0700

    feat: make CmsArrayOption, CmsArrayField, and typedefs generic on T

--- packages/dart_desk_annotation/lib/src/fields/complex/array_field.dart
+++ packages/dart_desk_annotation/lib/src/fields/complex/array_field.dart
@@ -2,27 +2,36 @@ import 'package:flutter/material.dart';
 
 import '../base/field.dart';
 
-abstract class CmsArrayOption extends CmsOption {
+typedef CmsArrayFieldItemBuilder<T> =
+    Widget Function(BuildContext context, T value);
+typedef CmsArrayFieldItemEditor<T> =
+    Widget Function(
+      BuildContext context,
+      T value,
+      ValueChanged<T>? onChanged,
+    );
+
+abstract class CmsArrayOption<T> extends CmsOption {
   const CmsArrayOption({super.hidden});
 
-  CmsArrayFieldItemBuilder get itemBuilder;
+  CmsArrayFieldItemBuilder<T> get itemBuilder;
 
   /// Override to provide a custom editor widget for array items.
-  /// When null, a default text-based editor will be used,
-  /// which stores items as String values.
-  CmsArrayFieldItemEditor? get itemEditor => null;
+  /// When null, a default editor will be used for primitive types
+  /// (String, num, int, double, bool).
+  CmsArrayFieldItemEditor<T>? get itemEditor => null;
 }
 
-class CmsArrayField extends CmsField {
+class CmsArrayField<T> extends CmsField {
   const CmsArrayField({
     required super.name,
     required super.title,
     super.description,
-    CmsArrayOption? super.option,
+    CmsArrayOption<T>? super.option,
   });
 
   @override
-  CmsArrayOption? get option => super.option as CmsArrayOption?;
+  CmsArrayOption<T>? get option => super.option as CmsArrayOption<T>?;
 }
 
 class CmsArrayFieldConfig<T extends Object?> extends CmsFieldConfig {
@@ -30,18 +39,9 @@ class CmsArrayFieldConfig<T extends Object?> extends CmsFieldConfig {
     super.name,
     super.title,
     super.description,
-    CmsArrayOption? super.option,
+    CmsArrayOption<T>? super.option,
   });
 
   @override
   List<Type> get supportedFieldTypes => [List];
 }
-
-typedef CmsArrayFieldItemBuilder =
-    Widget Function(BuildContext context, dynamic value);
-typedef CmsArrayFieldItemEditor =
-    Widget Function(
-      BuildContext context,
-      dynamic value,
-      ValueChanged<dynamic>? onChanged,
-    );
